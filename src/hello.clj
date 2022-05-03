480f562bec7b32c3848c46e043b465a391c0c7ae
Tokusei Noborio <tokusei@nyampass.com>
2022-05-03 21:31:57 +0900

    add counter

--- src/hello.clj
+++ src/hello.clj
@@ -2,27 +2,28 @@
   (:require [io.pedestal.http :as http]
             [io.pedestal.http.route :as route]))
 
-(defn respond-hello [request]
-  {:status 200 :body "Hello, world! 7"})
+(def counter (atom 0))
+
+(swap! counter inc)
+
+(defn respond-hello [_]
+  {:status 200 :body (str "Hello, world! " (swap! counter inc))})
 
 (def routes
   #(route/expand-routes
-   #{["/greet" :get respond-hello :route-name :greet]}))
+    #{["/greet" :get respond-hello :route-name :greet]}))
 
 (defn create-server []
   (-> {:env :dev
        ::http/routes routes
        ::http/type   :jetty
        ::http/host   "0.0.0.0"
-       ::http/port   8080
+       ::http/port   8899
        ::http/join? false}
       http/default-interceptors
       http/dev-interceptors
       http/create-server))
 
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn run [_]
   (http/start (create-server)))
-
-(run 3)
-
-
